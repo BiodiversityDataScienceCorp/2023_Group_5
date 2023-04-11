a9cbb90322b08ed4e84fca07f94a9ad89b9bf4db
n0liver21 <noliver@lclark.edu>
2023-04-11 19:03:11 +0000

    Small wording changes, adding a few comments, deleting unnecessary demo plots of current and future environmental conditions

--- scripts/futuresdm.R
+++ scripts/futuresdm.R
@@ -1,21 +1,19 @@
 
 # get climate data 
-currentEnv <- clim # renaming from the last sdm because clim isn't very specific
+currentEnv <- clim # renaming from the last sdm because "clim" isn't very specific
 
-#to set our specific future data
-
-#Can adjust the year by changing the "year" value, depending on how many years in the future you want to look. The options are 50 or 70.
-#Ours is year = 50, for 50 years into the future.
-#Can adjust the "rcp" value, depending on how you want to set your future climate predictions.
-#Ours is rcp = 45, which is a middle of the road value. You could raise this value for a more extreme climate prediction, or lower it for a less extreme prediction.
+# to set our specific future data...
+# Can adjust the year by changing the "year" value, depending on how many years in the future you want to look. 
+# The options are 50 or 70.
+# Ours is year = 50, for 50 years into the future.
+# Can adjust the "rcp" value, depending on how you want to set your future climate predictions.
+# Ours is rcp = 45, which is a middle-of-the-road value. You could raise this value for a more... 
+#... extreme climate prediction, or lower it for a less extreme prediction.
+# Make query for the future data and make it a raster
 futureEnv <- raster::getData(name = 'CMIP5', var = 'bio', res = 2.5,
                              rcp = 45, model = 'IP', year = 50, path="data") 
-
+# make future columnn names same as current ones
 names(futureEnv) = names(currentEnv)
-# look at current vs future climate vars
-plot(currentEnv[[1]])
-plot(futureEnv[[1]])
-
 
 # crop clim to the extent of the map you want
 geographicAreaFutureC5 <- crop(futureEnv, predictExtent)
@@ -25,7 +23,7 @@ geographicAreaFutureC5 <- crop(futureEnv, predictExtent)
 tortoisePredictPlotFutureC5 <- raster::predict(tortoiseSDM, geographicAreaFutureC5)  
 
 # for ggplot, we need the prediction to be a data frame 
-#convert output of ranapredictplot to a data frame
+#convert output of the tortoisePredictPlot to a data frame
 raster.spdfFutureC5 <- as(tortoisePredictPlotFutureC5, "SpatialPixelsDataFrame")
 tortoisePredictDfFutureC5 <- as.data.frame(raster.spdfFutureC5)
 
@@ -38,7 +36,7 @@ xmin <- min(tortoisePredictDfFutureC5$x)
 ymax <- max(tortoisePredictDfFutureC5$y)
 ymin <- min(tortoisePredictDfFutureC5$y)
 
-
+# make the final future SDM in ggplot:
 ggplot() +
   geom_polygon(data = wrld, mapping = aes(x = long, y = lat, group = group),
                fill = "grey75") +
@@ -55,8 +53,7 @@ ggplot() +
   theme(legend.box.background=element_rect(),legend.box.margin=margin(5,5,5,5)) 
 
 
-#Live ggsave here:
-
+# Save the final plot using ggsave:
 ggsave(filename="futureSDM.jpg", plot=last_plot(),path="output", width=1600, height=800, units="px")
 
 
